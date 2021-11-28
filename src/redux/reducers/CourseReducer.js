5774d517c00b5bab24d80cb3867bc1c2d71e6b43
shubhamk456 <shubham04mait@gmail.com>
2021-11-28 13:43:37 +0530

    Setup Reducer missing part

--- src/redux/reducers/CourseReducer.js
+++ src/redux/reducers/CourseReducer.js
@@ -1,7 +1,9 @@
 export default function courseReducer(state= [], action){
     switch(action.type){
         case "CREATE_COURSE":
-            return  [...state,{...action.course}]
+            return  [...state,{...action.course}];
+        default :
+           return state;
     }
 
 } 
